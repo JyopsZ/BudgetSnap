ab8c1f8a00ef1ef6888cf503c90a6d94398b1b9e
Adrian Villador <116620032+diannnnnnnnnnnnnnnnnnnnnnn@users.noreply.github.com>
2024-10-13 01:22:55 +0800

    MCO1 update 1
    Added homescreen (auto to onboarding1), addedd onboarding 1 -3 with intents

--- app/src/main/java/com/example/budgetsnap/onboarding3.java
+++ app/src/main/java/com/example/budgetsnap/onboarding3.java
@@ -2,7 +2,7 @@ package com.example.budgetsnap;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,31 +10,18 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class onboarding3 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_onboarding3);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        // automatically go to onboarding1 after 3 seconds
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                // intent to navigate to onboarding1
-                Intent i = new Intent(MainActivity.this, onboarding1.class);
-                startActivity(i);
-
-                // Optionally finish MainActivity so the user cannot return to it
-                finish();
-            }
-        }, 3000); // 3-second delay (can edit this to extend or shorten the delay)
-
     }
+
 }
