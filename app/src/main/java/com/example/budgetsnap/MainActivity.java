ac56db6eed0b97c658803e03876054d6cc1e3fe1
monicasantg <monica_santiago@dlsu.edu.ph>
2024-10-19 15:12:29 +0800

    fixed prof

--- app/src/main/java/com/example/budgetsnap/MainActivity.java
+++ app/src/main/java/com/example/budgetsnap/MainActivity.java
@@ -29,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
            @Override
             public void run() {
                 // intent to navigate to onboarding1
-                Intent i = new Intent(MainActivity.this, onboarding1.class);
+                Intent i = new Intent(MainActivity.this, account.class);
                 startActivity(i);
 
                 // Optionally finish MainActivity so the user cannot return to it
